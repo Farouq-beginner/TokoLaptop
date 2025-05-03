d74615a818c998786a4a2ad80d155f4777910dd3
Farouq-beginner <24111814081@mhs.unesa.ac.id>
2025-05-03 17:29:20 +0700

    style: tidy up code style

--- src/main/resources/static/js/parallax.js
+++ src/main/resources/static/js/parallax.js
@@ -5,72 +5,125 @@ window.addEventListener('scroll', function () {
 });
 
 
-if (window.innerWidth <= 768) {
-  gsap.set(["#m1", "#m2", "#t2", "#t1", "#man", "#plants"], { y: 0, x: 0 });
-}
-// Parallax Scrolling for Background Elements
 if (window.innerWidth > 768) {
   gsap.from("#m1", {
     scrollTrigger: {
-      scrub: 1,
+      scrub: 4, // Scrub lebih tinggi untuk efek lebih dramatis
       trigger: "#m1",
       start: "top bottom",
       end: "bottom top",
     },
-    y: 10,
+    y: 60,
+    scale: 1.2,
+    rotate: 3, 
+    skewX: 5, // Efek miring untuk perspektif lebih dinamis
+    duration: 4,
+    ease: "power2.out",
   });
 
   gsap.from("#m2", {
     scrollTrigger: {
-      scrub: 1,
+      scrub: 4,
       trigger: "#m2",
       start: "top bottom",
       end: "bottom top",
     },
-    y: 50,
+    y: 120,
+    scale: 1.25,
+    rotate: -3,
+    skewX: -5,
+    duration: 4.2,
+    ease: "power2.out",
   });
 
   gsap.from("#t2", {
     scrollTrigger: {
-      scrub: 1,
+      scrub: 4,
       trigger: "#t2",
       start: "top bottom",
       end: "bottom top",
     },
-    x: -50,
+    x: -120,
+    scale: 1.15,
+    skewY: 3,
+    duration: 4.5,
+    ease: "power3.out",
   });
 
   gsap.from("#t1", {
     scrollTrigger: {
-      scrub: 1,
+      scrub: 4,
       trigger: "#t1",
       start: "top bottom",
       end: "bottom top",
     },
-    x: 50,
+    x: 120,
+    scale: 1.15,
+    skewY: -3,
+    duration: 4.5,
+    ease: "power3.out",
   });
 
   gsap.from("#man", {
     scrollTrigger: {
-      scrub: 1,
+      scrub: 4,
       trigger: "#man",
       start: "top bottom",
       end: "bottom top",
     },
-    x: -250,
+    x: -400,
+    opacity: 0,
+    scale: 1.3,
+    skewX: 10,
+    duration: 5,
+    ease: "power3.out",
   });
 
   gsap.from("#plants", {
     scrollTrigger: {
-      scrub: 1,
+      scrub: 4,
       trigger: "#plants",
       start: "top bottom",
       end: "bottom top",
     },
-    x: -50,
+    x: -100,
+    scale: 1.1,
+    skewY: 5,
+    duration: 4.3,
+    ease: "power2.out",
   });
+
+  gsap.from("#text1", {
+    scrollTrigger: {
+      scrub: 4,
+      trigger: "#text1",
+      start: "top bottom",
+      end: "bottom top",
+    },
+    y: 70,
+    x: -100, // Geser ke kiri sebanyak 50px
+    opacity: 0,
+    scale: 1.2,
+    duration: 4,
+    ease: "power2.out",
+  });
+
+  gsap.from("#text", {
+    scrollTrigger: {
+      scrub: 4,
+      trigger: "#text",
+      start: "top bottom",
+      end: "bottom top",
+    },
+    y: 70,
+    x: -100, // Geser ke kiri sebanyak 50px
+    opacity: 0,
+    scale: 1.2,
+    duration: 4,
+    ease: "power2.out",
+});
 } else {
-  gsap.set(["#m1", "#m2", "#t2", "#t1", "#man", "#plants"], { clearProps: "all" });
+  gsap.set(["#m1", "#m2", "#t2", "#t1", "#man", "#plants", "#text1", "#text"], { clearProps: "all" });
 }
 
 
