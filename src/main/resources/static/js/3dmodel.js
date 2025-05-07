ca8818aed536d08ac824fdbf18567fb88838d28f
Farouq-beginner <24111814081@mhs.unesa.ac.id>
2025-05-07 09:11:48 +0700

    feat: add 3D model viewer and enhance laptop UI styles

---
+++ src/main/resources/static/js/3dmodel.js
@@ -0,0 +1,6 @@
+window.onload = function() {
+    const modelContainer = document.getElementById('model-viewer-container');
+    modelContainer.classList.remove('hidden');
+    modelContainer.classList.add('visible');
+  };
+  
--- src/main/resources/static/js/cards.js
+++ src/main/resources/static/js/cards.js
@@ -1,29 +1,88 @@
-const cards = document.querySelector(".cards");
-    const images = document.querySelectorAll(".card__img");
-    const backgrounds = document.querySelectorAll(".card__bg");
-    const range = 40;
+Vue.component('card', {
+  template: `
+                <div class="card-wrap"
+                  @mousemove="handleMouseMove"
+                  @mouseenter="handleMouseEnter"
+                  @mouseleave="handleMouseLeave"
+                  ref="card">
+                  <div class="card"
+                    :style="cardStyle">
+                    <div class="card-bg" :style="[cardBgTransform, cardBgImage]"></div>
+                    <div class="card-info">
+                      <slot name="header11"></slot>
+                      <slot name="content11"></slot>
+                    </div>
+                  </div>
+                </div>`,
+  mounted() {
+    this.width = this.$refs.card.offsetWidth;
+    this.height = this.$refs.card.offsetHeight;
+  },
+  props: {
+    dataImage: {
+      type: String,
+      required: true
+    }
+  },
+  data() {
+    return {
+      width: 0,
+      height: 0,
+      mouseX: 0,
+      mouseY: 0,
+      mouseLeaveDelay: null
+    };
+  },
+  computed: {
+    mousePX() {
+      return this.mouseX / this.width;
+    },
+    mousePY() {
+      return this.mouseY / this.height;
+    },
+    cardStyle() {
+      const rX = Math.min(Math.max(this.mousePX * 25, -25), 25);
+      const rY = Math.min(Math.max(this.mousePY * -25, -25), 25);
+      return {
+        transform: `rotateY(${rX}deg) rotateX(${rY}deg)`
+      };
+    },
+    cardBgTransform() {
+      const tX = this.mousePX * -20; // Kurangi nilai agar gambar tidak keluar
+      const tY = this.mousePY * -20;
+      return {
+          transform: `translateX(${tX}px) translateY(${tY}px)`
+      };
+  },
+  
+  cardBgImage() {
+    return {
+        backgroundImage: `url(${this.dataImage})`,
+        backgroundSize: 'cover', // Pastikan gambar memenuhi layar
+        backgroundPosition: 'center',
+        backgroundRepeat: 'no-repeat'
+    };
+}
 
-    // const calcValue = (a, b) => (((a * 100) / b) * (range / 100) -(range / 2)).toFixed(1);
-    const calcValue = (a, b) => (a / b * range - range / 2).toFixed(1) // thanks @alice-mx
+  },
+  methods: {
+    handleMouseMove(e) {
+      const bounds = this.$refs.card.getBoundingClientRect();
+      this.mouseX = e.clientX - bounds.left - bounds.width / 2;
+      this.mouseY = e.clientY - bounds.top - bounds.height / 2;
+    },
+    handleMouseEnter() {
+      clearTimeout(this.mouseLeaveDelay);
+    },
+    handleMouseLeave() {
+      this.mouseLeaveDelay = setTimeout(() => {
+        this.mouseX = 0;
+        this.mouseY = 0;
+      }, 1000);
+    }
+  }
+});
 
-    let timeout;
-    document.addEventListener('mousemove', ({ x, y }) => {
-      if (timeout) {
-        window.cancelAnimationFrame(timeout);
-      }
-
-      timeout = window.requestAnimationFrame(() => {
-        const yValue = calcValue(y, window.innerHeight);
-        const xValue = calcValue(x, window.innerWidth);
-
-        cards.style.transform = `rotateX(${yValue}deg) rotateY(${xValue}deg)`;
-
-        [].forEach.call(images, (image) => {
-          image.style.transform = `translateX(${-xValue}px) translateY(${yValue}px)`;
-        });
-
-        [].forEach.call(backgrounds, (background) => {
-          background.style.backgroundPosition = `${xValue * .45}px ${-yValue * .45}px`;
-        })
-      })
-    }, false);
+const app = new Vue({
+  el: '#app'
+});
